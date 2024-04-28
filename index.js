0a5fc55288b982aa63f17f32e541188d29c193d0
musfiqrabby <musfiqrabby69@gmail.com>
2024-04-28 11:09:26 +0600

    view details id added to mongodb

--- index.js
+++ index.js
@@ -46,6 +46,16 @@ async function run() {
       res.send(result);
     })
 
+
+    app.get('/users/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await usersCollection.findOne(query);
+      res.send(result);
+    })
+
+
+    
     app.get('/artcraft/:email', async(req, res) => {
       console.log(req.params.email)
       const result = await usersCollection.find({email:req.params.email}).toArray();
