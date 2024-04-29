f78c5ad8ff3a89b21ff4a7dd95a64269e8bf36a6
musfiqrabby <musfiqrabby69@gmail.com>
2024-04-29 19:01:34 +0600

    Craft Items Section

--- index.js
+++ index.js
@@ -45,8 +45,13 @@ async function run() {
       const result = await usersCollection.findOne(query);
       res.send(result);
     })
+    
+    app.get("/featuredSixCrafts", async(req,res)=>{
+      const data = await usersCollection.find().limit(6).toArray()
+      res.send(data)
+      })
 
-
+      
     app.get('/users/:id', async(req, res) =>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
